e9c33494f6f08bc316b8bb02c4f1bd60cafacb89
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-05-06 00:17:01 -0300

    feat: adiciona a lib swiper e algumas logos de clientes

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    468: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
